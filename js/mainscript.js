2c572b7f2e63becbc9cb84963c50f2494d9a6d18
smt <smt_razrab@mail.ru>
2023-01-31 22:37:41 +0500

    js

--- js/mainscript.js
+++ js/mainscript.js
@@ -33,6 +33,8 @@ function SlideScroll(event) {
 	})
 }
 
+setInterval(() => SlideScroll('right'), 5000)
+
 
 
 
@@ -46,3 +48,84 @@ drop.addEventListener('click', () => {
 	drop.classList.toggle('active')
 	list.classList.toggle('active')
 })
+
+
+
+
+const arrowLeft1 = document.querySelector('#arrowLeft1')
+const arrowRight1 = document.querySelector('#arrowRight1')
+const slides1 = document.querySelectorAll('.cakes__slides')
+const slidesCount1 = document.querySelectorAll('.cakes__slides').length
+
+let activeSlideIndex1 = 0
+
+arrowLeft1.addEventListener('click', () => {
+	SlideScroll1('left')
+})
+arrowRight1.addEventListener('click', () => {
+	SlideScroll1('right')
+})
+
+
+function SlideScroll1(event) {
+	if (event === 'right') {
+		activeSlideIndex1++
+		if (activeSlideIndex1 === slidesCount1) {
+			activeSlideIndex1 = 0
+		}
+	} else if (event === 'left') {
+		activeSlideIndex1--
+		if (activeSlideIndex1 < 0) {
+			activeSlideIndex1 = slidesCount1 - 1
+		}
+	}
+
+	width = slides1[activeSlideIndex1].clientWidth
+
+	slides1.forEach(slide => {
+		slide.style.transform = `translateX(-${activeSlideIndex1 * width}px)`
+	})
+}
+
+
+setInterval(() => SlideScroll1('right'), 5000)
+
+
+
+const arrowLeft2 = document.querySelector('#arrowLeft2')
+const arrowRight2 = document.querySelector('#arrowRight2')
+const slides2 = document.querySelectorAll('.cupcakes__slides')
+const slidesCount2 = document.querySelectorAll('.cupcakes__slides').length
+
+let activeSlideIndex2 = 0
+
+arrowLeft2.addEventListener('click', () => {
+	SlideScroll2('left')
+})
+arrowRight2.addEventListener('click', () => {
+	SlideScroll2('right')
+})
+
+
+function SlideScroll2(event) {
+	if (event === 'right') {
+		activeSlideIndex2++
+		if (activeSlideIndex2 === slidesCount2) {
+			activeSlideIndex2 = 0
+		}
+	} else if (event === 'left') {
+		activeSlideIndex2--
+		if (activeSlideIndex2 < 0) {
+			activeSlideIndex2 = slidesCount2 - 1
+		}
+	}
+
+	width = slides2[activeSlideIndex2].clientWidth
+
+	slides2.forEach(slide => {
+		slide.style.transform = `translateX(-${activeSlideIndex2 * width}px)`
+	})
+}
+
+
+setInterval(() => SlideScroll2('right'), 5000)
